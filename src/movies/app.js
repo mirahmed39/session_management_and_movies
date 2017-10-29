67110849bb2d9e5f63e6fc791dad3048f3edad02
Mir Ahmed <mir.ahmed39@yahoo.com>
2017-10-29 14:36:13 -0400

    finshed part 5 of movies

--- src/movies/app.js
+++ src/movies/app.js
@@ -1,1 +1,43 @@
 // app.js
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const db = require('./db');
+const Movie = mongoose.model('Movie');
+const path = require('path');
+const publicPath = path.resolve(__dirname, 'public');
+
+app.set('view engine', 'hbs');
+app.use(express.static(publicPath));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/movies', function (req, res) {
+    const directorName = req.query.director;
+    if (directorName !== undefined && directorName  !== "") {
+        Movie.find({director: directorName}, function (err, movies, count) {
+            res.render('movies', {movies: movies});
+        });
+    } else {
+        Movie.find(function (err, movies, count) {
+            res.render('movies', {movies: movies});
+        });
+    }
+});
+
+app.get('/movies/add', function (req, res) {
+    res.render('addMovie', {});
+});
+
+app.post('/movies/add', function (req, res) {
+    const title = req.body.title;
+    const director = req.body.director;
+    const year = req.body.year;
+    const movie = new Movie({title: title, year: year, director:director});
+    movie.save(function (err, movie, count) {
+        console.log("Movie added successfully!!!");
+        res.redirect('/movies');
+    });
+});
+
+app.listen(3000);
--- src/movies/db.js
+++ src/movies/db.js
@@ -1,1 +1,15 @@
 // db.js
+const mongoose = require('mongoose');
+
+// my schema goes here
+const Movie = new mongoose.Schema({
+    title: String,
+    year: Number,
+    director: String
+});
+
+// registering model
+mongoose.model('Movie', Movie);
+
+// coonect to the database
+mongoose.connect('mongodb://localhost/hw05');
